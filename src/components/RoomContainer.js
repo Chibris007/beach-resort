fb525ff319716e816a8594acb5705c6a3c3bcc3f
Chibris007 <eihejirika4900@gmail.com>
2020-02-17 18:54:58 +0100

    react beach application

---
+++ src/components/RoomContainer.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import RoomFilter from './RoomFilter';
+import RoomList from './RoomList';
+import {withRoomConsumer} from '../context';
+//import Loading from './Loading';
+
+function RoomContainer( { context }) {
+    const { sortedRooms, rooms} = context;
+    //if (loading) {
+       // return <Loading />;
+    //}
+        return (
+            <div>
+                
+                <RoomFilter  rooms={rooms} />
+                <RoomList rooms={sortedRooms} />
+                
+            </div>
+        );
+    }
+
+export default withRoomConsumer(RoomContainer);
+
+
+
+
+/*import React from 'react';
+import RoomFilter from './RoomFilter';
+import RoomList from './RoomList';
+import {RoomConsumer} from '../context';
+//import Loading from './Loading';
+
+
+export default function RoomContainer() {
+    return (
+        <RoomConsumer>
+            {value => {
+                const {Loading, sortedRooms, rooms} = value
+                //if (loading) {
+                   // return <Loading />;
+                //}
+                    return (
+                        <div>
+                            Hello from Room Container
+                            <RoomFilter  rooms={rooms} />
+                            <RoomList rooms={sortedRooms} />
+                            
+                        </div>
+                    );
+                }}
+        </RoomConsumer>
+    );
+}*/         
